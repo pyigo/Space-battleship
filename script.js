451b538eb8c23cccb3c791c439ab1f3e590dd79a
Paulette YIGO <paulette@Paulettes-MacBook-Air.local>
2021-12-12 02:02:14 -0500

    starting to cerate a uss ship and alienships

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+//batleship class creation
 class Ship {
     constructor(hull, firepower, accuracy) {
         this.hull = hull
         this.firepower = firepower
         this.accuracy = accuracy
     }
-
-
 }
+// creating uSS HelloWorld battleship
+let ussHelloWorld = new Ship(20, 5, 0.7);
+
+// Copied form https://www.w3schools.com/js/js_random.asp
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//   creating alienship 
+let alienships = [];
+let hull = getRndInteger(3, 6);
+let firepower = getRndInteger(2, 4);
+let accuracy = getRndInteger(0.6, 0.8);
+let alienshipOne = new Ship();
+
+
+
