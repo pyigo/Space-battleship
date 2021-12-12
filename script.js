dce528387c995e3e4c5e554719d60814433d8b1b
Paulette YIGO <paulette@Paulettes-MacBook-Air.local>
2021-12-12 15:36:58 -0500

    added console log to see object creation

--- script.js
+++ script.js
@@ -5,13 +5,24 @@ class Ship {
         this.firepower = firepower
         this.accuracy = accuracy
     }
+
+    // creating display method to display details of class Ship
+    Display() {
+        console.log(`hull:${this.hull}, firepower:${this.firepower}, accuracy:${this.accuracy}`)
+    }
 }
 // creating uSS HelloWorld battleship
 let ussHelloWorld = new Ship(20, 5, 0.7);
+console.log(`USS Helloworld details:`);
+ussHelloWorld.Display();
 
 // Copied form https://www.w3schools.com/js/js_random.asp
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//https://stackoverflow.com/questions/17726753/get-a-random-number-between-0-0200-and-0-120-float-numbers
+function getRndFloating(min, max) {
+    return (Math.random() * (max - min) + min).toFixed(1);
 }
 
 // let's create a function that create and return a alienship
@@ -19,37 +30,31 @@ function createAlienship() {
     //generte random numbers for each property hull,firepower, accuracy 
     let hull = getRndInteger(3, 6);
     let firepower = getRndInteger(2, 4);
-    let accuracy = getRndInteger(0.6, 0.8);
+    let accuracy = getRndFloating(0.6, 0.8);
 
-    //create alienship1 
+    //create alienship
     let alienship = new Ship(hull, firepower, accuracy);
 
     return alienship;
 }
-
 //   creating alienship array to contain all 6 ships  
 let alienships = [];
 
-// calling FUNCTION createAlienship() for every new ship
-let alienshipOne = createAlienship();
-alienships.push(alienshipOne);
+// create for loop to populate array alienships
+for (let i = 0; i < 6; i++) {
+    let alienship = createAlienship();
+    alienships.push(alienship);
+}
 
-let alienshipTwo = createAlienship();
-alienships.push(alienshipTwo);
+// alienships[1].Display();
+// alienships[0].Display();
+console.log(`Alienships details`)
+// creating for loop to display object properties
+for (let i = 0; i < alienships.length; i++) {
+    alienships[i].Display();
+}
 
-let alienshipThree = createAlienship();
-alienships.push(alienshipThree);
-
-let alienshipFour = createAlienship();
-alienships.push(alienshipFour);
-
-let alienshipFive = createAlienship();
-alienships.push(alienshipFive);
-
-let alienshipSix = createAlienship();
-alienships.push(alienshipSix);
-
-
+console.log('********Game********')
 
 
 
