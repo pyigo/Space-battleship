de0918c73ac78bebf2cc57ce10086751c0972c83
Paulette YIGO <paulette@Paulettes-MacBook-Air.local>
2021-12-12 12:29:56 -0500

    replaced manual ship creation by function

--- script.js
+++ script.js
@@ -14,58 +14,6 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//   creating alienship array to contain all 6 ships  
-let alienships = [];
-
-
-
-//push created ship to aarray
-alienships.push(alienshipOne);
-
-// create var alienship2
-// generate a new value for each property
-hull = getRndInteger(3, 6);
-firepower = getRndInteger(2, 4);
-accuracy = getRndInteger(0.6, 0.8);
-let alienshipTwo = new Ship(hull, firepower, accuracy);
-
-// now push alienship2 in array
-alienships.push(alienshipTwo);
-
-
-// do same for each alienship ennemy
-// generate new value for each property
-// then push each new ship inside array
-
-hull = getRndInteger(3, 6);
-firepower = getRndInteger(2, 4);
-accuracy = getRndInteger(0.6, 0.8);
-let alienshipThree = new Ship(hull, firepower, accuracy);
-
-alienships.push(alienshipThree);
-
-
-hull = getRndInteger(3, 6);
-firepower = getRndInteger(2, 4);
-accuracy = getRndInteger(0.6, 0.8);
-let alienshipFour = new Ship(hull, firepower, accuracy);
-
-alienships.push(alienshipFour);
-
-hull = getRndInteger(3, 6);
-firepower = getRndInteger(2, 4);
-accuracy = getRndInteger(0.6, 0.8);
-let alienshipFive = new Ship(hull, firepower, accuracy);
-
-alienships.push(alienshipFive);
-
-hull = getRndInteger(3, 6);
-firepower = getRndInteger(2, 4);
-accuracy = getRndInteger(0.6, 0.8);
-let alienshipSix = new Ship(hull, firepower, accuracy);
-
-alienships.push(alienshipSix);
-
 // let's create a function that create and return a alienship
 function createAlienship() {
     //generte random numbers for each property hull,firepower, accuracy 
@@ -78,6 +26,30 @@ function createAlienship() {
 
     return alienship;
 }
+
+//   creating alienship array to contain all 6 ships  
+let alienships = [];
+
+// calling FUNCTION createAlienship() for every new ship
+let alienshipOne = createAlienship();
+alienships.push(alienshipOne);
+
+let alienshipTwo = createAlienship();
+alienships.push(alienshipTwo);
+
+let alienshipThree = createAlienship();
+alienships.push(alienshipThree);
+
+let alienshipFour = createAlienship();
+alienships.push(alienshipFour);
+
+let alienshipFive = createAlienship();
+alienships.push(alienshipFive);
+
+let alienshipSix = createAlienship();
+alienships.push(alienshipSix);
+
+
 
 
 
